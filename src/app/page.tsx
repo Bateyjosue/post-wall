777f2672d065e31c9c0f30c13978b42af84ff590
Bateyjosue <josuebatey19@gmail.com>
2025-07-03 14:48:48 +0200

    Set up Next.js project with initial configuration, including ESLint, PostCSS, and Tailwind CSS. Added basic layout and homepage structure, along with SVG assets and global styles.

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-red-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-700">Hello, Post Wall!</h1>
+    </main>
+  );
+}
